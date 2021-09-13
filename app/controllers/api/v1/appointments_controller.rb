25d396c2a5dba1e1b15fb9bb69e0009f0da64c82
Anthony Lebro <antho.devjam@gmail.com>
2021-09-13 11:22:28 +0200

    Updating Controller Appointment

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::AppointmentsController < Api::BaseController
 
-  before_action :find_appointment, only: %w[show]
+  before_action :find_appointment, only: %w[show update destroy]
+
 
   def create
-    @appointment = Appointment.new(appointment_params)
+    @appointment = Appointment.new(appointment_params())
     @appointment.nutritionist = current_user
+    @appointment.BMI = @appointment.weight/(@appointment.height**)
 
     if @appointment.save 
       render json: @appointment, status: :created
@@ -23,12 +25,16 @@ class Api::V1::AppointmentsController < Api::BaseController
   end
 
   def update
-    @appointment = Appointment.update
-    render json: Appointment.new
+  if  @appointment.update(appointment_params())
+    render json:@appointment, status: :success
+  else 
+    render json:@appointment.errors, status: :errors
+
   end
 
   def destroy
     @appointment = Appointment.destroy
+    render json:{"Supprimé"}
   end
 
 
@@ -39,7 +45,7 @@ class Api::V1::AppointmentsController < Api::BaseController
   end
 
   def appointment_params
-    params.require(:appointment).permit(:date, :weight, :height, :body_fat, :muscle_mass, :visceral_fat, :content, :patient_id, :nutritionist_id)
+    params.require(:appointment).permit(:date, :weight, :height, :body_fat, :muscle_mass, :visceral_fat, :content, :patient_id, :nutritionist_id, :avatar)
   end
 
 end
