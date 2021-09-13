d70328763e4feabac5ea6dbac00b683a0ba3845e
Xabi <xabi.aycaguer@gmail.com>
2021-09-13 18:03:16 +0200

    add nutritionists model tests

--- spec/models/nutritionist_spec.rb
+++ spec/models/nutritionist_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Nutritionist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(create_nutritionist())
+      .to be_valid
+  end
+
+  it "is not valid without content" do
+    nutritionist2 = build(:nutritionist,content: nil)
+    expect(nutritionist2).to_not be_valid
+  end
 end
