b8f64d93cf1e8992ff40cd90b2da9f52ab05192b
Xabi <xabi.aycaguer@gmail.com>
2021-09-13 10:37:56 +0200

    create password controllers for patient & nutritionist

--- app/models/nutritionist.rb
+++ app/models/nutritionist.rb
@@ -12,4 +12,28 @@ class Nutritionist < ApplicationRecord
   validates :api_key, presence: true, uniqueness: true
   validates :slug_calendly, presence: true, format: { with: /https:\/\/calendly.com\/.*/ }, uniqueness: true
 
+
+
+  def generate_password_token!
+    self.reset_password_token = generate_token
+    self.reset_password_sent_at = Time.now.utc
+    save!
+  end
+
+  def password_token_valid?
+    (self.reset_password_sent_at + 4.hours) > Time.now.utc
+  end
+
+  def reset_password!(password)
+    self.reset_password_token = nil
+    self.password = password
+    save!
+  end
+
+  private
+
+  def generate_token
+    SecureRandom.hex(10)
+  end
+
 end
---
+++ spec/requests/nutritionist_passwords_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "NutritionistPasswords", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
