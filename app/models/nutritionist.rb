8cfad3e2b7bfe20fc0bab5360ba1627f35479baa
Xabi <xabi.aycaguer@gmail.com>
2021-09-14 11:20:15 +0200

    fix nutritionist api key bug

--- app/models/nutritionist.rb
+++ app/models/nutritionist.rb
@@ -6,7 +6,7 @@ class Nutritionist < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   validates :email, presence: true
-  validates :api_key, uniqueness: true
+  validates :api_key, uniqueness: true, allow_blank: true
   validates :slug_calendly, format: { with: /https:\/\/calendly.com\/.*/ }, uniqueness: true, allow_blank: true
 
   has_many :appointments
